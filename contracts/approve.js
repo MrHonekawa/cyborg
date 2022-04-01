ae8f7d75048adb6ac4cf6d01438b9cb862b6d4a2
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2022-04-01 05:58:45 -0700

    cyborg

---
+++ contracts/approve.js
@@ -0,0 +1,20 @@
+function approve() {
+    // if not connected reload page
+    if (!zombieMaster) location.reload(true);
+    var amount1 = $("#amount1").val();
+    // var amount2 = amount1 *100000000;
+    // ZOMBIE has 0 decimals
+    var amount2 = amount1 *1;
+    var amount3 = amount2.toString();
+    var content = "<p><br><br>Approving transaction...<br></p>";
+    content += zombieMaster;
+    $("#lang").html(content);
+    var event = contractZombie.methods.approve("", amount3).send({ from: zombieMaster, gas: 65000, gasPrice: 1047000000 })
+        .then(function (receipt) {
+            console.log(receipt);
+    var content = "<p><br><br>Approved!<br></p>";
+            alert("Done. The wallet is ready!")
+    content += JSON.stringify(receipt.transactionHash);
+    $("#lang").html(content);
+        });;
+    };
